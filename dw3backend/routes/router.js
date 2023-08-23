119721c4839f44a6c8a537b8646c1c4661984935
Veterano <marina.n@aluno.ifsp.edu.br>
2023-08-23 11:11:44 -0300

    BackendAlunos

--- dw3backend/routes/router.js
+++ dw3backend/routes/router.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const routerApp = express.Router();
 
+const appAlunos = require("../apps/alunos/controller/ctlAlunos");
+const appLogin = require("../apps/login/controller/ctlLogin");
+
 // middleware that is specific to this router
 routerApp.use((req, res, next) => {
-    next();
+  next();
 });
 
 routerApp.get("/", (req, res) => {
-    res.send("Olá mundo!");
+  res.send("Olá mundo!");
 });
 
-//@ Rotas de Alunos
-
-//@ Rotas de Cursos
+//Rotas de Alunos
+routerApp.get("/getAllAlunos", appAlunos.getAllAlunos);
+routerApp.post("/getAlunoByID", appLogin.AutenticaJWT, appAlunos.getAlunoByID);
 
 // Rota Login
+routerApp.post("/Login", appLogin.Login);
+routerApp.post("/Logout", appLogin.Logout);
 
 module.exports = routerApp;
