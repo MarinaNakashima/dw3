487ff94e300307c756f0814b5aadf8e89429df89
Veterano <marina.n@aluno.ifsp.edu.br>
2023-09-27 10:30:20 -0300

    FrontendNode

--- dw3frontNode/app.js
+++ dw3frontNode/app.js
@@ -11,6 +11,8 @@ const session = require('express-session');
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
+var alunosRouter = require('./routes/rte_alunos');
+var cursosRouter = require('./routes/rte_cursos');
 
 
 var app = express();
@@ -20,38 +22,40 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'vash');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+ app.use(logger('dev'));
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: false }));
+ app.use(cookieParser());
 
-app.use(
-  session({
-    secret: "palavrasecreta",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: null },
-  })
-);
+ app.use(
+ session({
+ secret: "palavrasecreta",
+ resave: false,
+ saveUninitialized: true,
+ cookie: { maxAge: null },
+ })
+ );
 
-app.use(express.static(path.join(__dirname, 'public')));
+ app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+ app.use('/', indexRouter);
+ app.use('/alunos', alunosRouter);
+ app.use('/cursos',  cursosRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+ // catch 404 and forward to error handler
+ app.use(function(req, res, next) {
+   next(createError(404));
+ });
+  
+ // error handler
+ app.use(function(err, req, res, next) {
+ // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+ });
 
-module.exports = app;
+ module.exports = app;
